d035e4a74647e2e3249d6b8ff85851f891b03547
soregashi-27 <tomo.soul.15@gmail.com>
2021-07-31 00:11:59 +0900

    webpackビルド

---
+++ components/example.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="example">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue.js!(2.6.14)',
+    };
+  },
+};
+</script>
+
+<style></style>
